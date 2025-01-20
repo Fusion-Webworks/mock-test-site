1ee7a09405d20a4fa62ce28aaac229421f08b371
Yogesh832e <upadhayayyogesh832@gmail.com>
2025-01-20 15:54:12 +0530

    basic structure completed

--- backend/routers/auth.routes.js
+++ backend/routers/auth.routes.js
@@ -5,13 +5,8 @@ const authController = require("../controllers/auth.controller");
 
 router.post("/register", authController.register);
 router.post("/login", authController.login);
-<<<<<<< HEAD
 router.get("/login", authController.login);
 router.get('/profile', verifyToken, authController.profile); 
 // router.post("/logout", logout);
-=======
-router.get('/profile/:_id', verifyToken, authController.profile); 
-
->>>>>>> 86e34f560cbd3401c78b3053e83defcca35f8d00
 
 module.exports = router;
