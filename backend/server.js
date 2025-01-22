77eccfd1772a53ec0d9817f1b7f17ccf4d4d8913
Yogesh832e <upadhayayyogesh832@gmail.com>
2025-01-22 12:09:32 +0530

    checkiing and testing

--- backend/server.js
+++ backend/server.js
@@ -13,23 +13,21 @@ connectDB();
 
 //middleware
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.use(cors());
 
 //admin auth route
-app.use("/api/v1/auth" , authRoutes);
+app.use("/api/v1/auth", authRoutes);
 
 //admin text route
-app.use("/api/v1/adminText" , textRoutes);
-
-
+app.use("/api/v1/adminText", textRoutes);
 
 app.get("/", (req, res) => {
   res.json({
     message: "API is running...",
   });
 });
-
-
 
 app.listen(PORT, () => {
   console.log(`Server is running at http://localhost:${PORT}`);
