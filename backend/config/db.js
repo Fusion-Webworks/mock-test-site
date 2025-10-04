61abdf0c57603e3b6c232044d8205546f3bd03b0
Yogesh832e <upadhayayyogesh832@gmail.com>
2025-10-04 16:29:31 +0530

    update

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,9 +1,10 @@
 const mongoose = require("mongoose");
 
-
 async function connectDB() {
   try {
-    await mongoose.connect("mongodb+srv://leeladharupadhayay7:test123@cluster0.5zor9.mongodb.net/mocktest?retryWrites=true&w=majority&appName=Cluster0");
+    await mongoose.connect(
+      "mongodb+srv://leeladharupadhayay7:leeladharupadhayay7@cluster0.5zor9.mongodb.net/mocktest?retryWrites=true&w=majority&appName=Cluster0"
+    );
     //database connection
     console.log("MongoDB is connected successfully!");
   } catch (error) {
